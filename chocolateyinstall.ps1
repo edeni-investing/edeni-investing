2d9408749eced0bed999607e212a8e6f6a98a15e
Eden Iskovich <edeni@yukon.local>
2021-10-27 12:54:59 +0300

    .

---
+++ chocolateyinstall.ps1
@@ -0,0 +1,48 @@
+﻿Start-Process powershell -verb runas -ArgumentList "-file fullpathofthescript"
+Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
+# 3. In Chocolatey scripts, ALWAYS use absolute paths - $toolsDir gets you to the package's tools directory.
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'TLVOVPN.msi'
+# If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
+#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
+# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
+# Internal/Organization: Download from internal location (internet sources are unreliable)
+
+$packageArgs = @{
+  packageName   = 'openvpntlv'
+  unzipLocation = $toolsDir
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  file         = $fileLocation
+
+  softwareName  = 'openvpntlv*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  # Checksums are now required as of 0.10.0.
+  # To determine checksums, you can get that from the original site if provided.
+  # You can also use checksum.exe (choco install checksum) and use it
+  # e.g. checksum -t sha256 -f path\to\file
+  checksum      = ''
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = ''
+  checksumType64= 'sha256' #default is checksumType
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+  # OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  #silentArgs   = '/S'           # NSIS
+  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s'           # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+                                 #       https://community.chocolatey.org/packages/autohotkey.portable
+  #validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+Install-ChocolateyInstallPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
